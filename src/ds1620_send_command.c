ff7c95155ab0dd8dbc1820bd2b3cc249fed59eb3
Nick Nerovny <nikolay.nerovniy@gmail.com>
2018-07-07 21:04:57 +0300

    Added tests for ds1620_send_command()

--- src/ds1620_send_command.c
+++ src/ds1620_send_command.c
@@ -6,12 +6,13 @@ void ds1620_send_command(int command,
   int n;
   int bit;
 
-  for (n = 0; n < 8; n++) {     // Always send 8 bits of data
-    // Arithmetic bitwise shift command to the right, then AND with
-    // bitmask (00000001) to return next bit, least significant (rightmost) first
-    bit = (command >> n) & 0x01;
-    callbacks->dq_set_callback(bit);       //digitalWrite(_DQ, bit); // DQ HIGH or LOW based on bit
-    callbacks->clock_low_callback();       //digitalWrite(_CLK, LOW);  // CLK LOW then HIGH to make one cycle
-    callbacks->clock_high_callback();      //digitalWrite(_CLK, HIGH);  
-  }
+  if(callbacks!=0)
+    for (n = 0; n < 8; n++) {     // Always send 8 bits of data
+      // Arithmetic bitwise shift command to the right, then AND with
+      // bitmask (00000001) to return next bit, least significant (rightmost) first
+      bit = (command >> n) & 0x01;
+      callbacks->dq_set_callback(bit);       //digitalWrite(_DQ, bit); // DQ HIGH or LOW based on bit
+      callbacks->clock_low_callback();       //digitalWrite(_CLK, LOW);  // CLK LOW then HIGH to make one cycle
+      callbacks->clock_high_callback();      //digitalWrite(_CLK, HIGH);  
+    }
 }
--- test/test_ds1620_send_command.c
+++ test/test_ds1620_send_command.c
@@ -1,15 +1,69 @@
 #include "unity.h"
 #include "ds1620_send_command.h"
+#include "mock_example.h"
+#include <string.h>
+
+struct ds1620_callbacks callbacks;
 
 void setUp(void)
 {
+  callbacks.clock_low_callback = example_clock_low;
+  callbacks.clock_high_callback = example_clock_high;
+  callbacks.reset_low_callback = example_reset_low;
+  callbacks.reset_high_callback = example_reset_high;
+  callbacks.dq_set_callback = example_dq_set;
+  callbacks.dq_get_callback = example_dq_get;
+  callbacks.dq_set_output_callback = example_dq_set_output;
+  callbacks.dq_set_input_callback = example_dq_set_input;
+  callbacks.setup_ports_callback = example_setup_ports;
+  callbacks.delay_callback = example_delay;
 }
 
 void tearDown(void)
 {
+  memset(&callbacks,0,sizeof(callbacks));
 }
 
-void test_ds1620_send_command_NeedToImplement(void)
+void test_ds1620_send_command_null_pointer_should_not_segfault(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement ds1620_send_command");
+  ds1620_send_command(0,0);
+}
+
+void test_ds1620_send_command_should_work_all_zeroes(void)
+{
+  for(uint8_t i=0;i<8;++i)
+  {
+    example_dq_set_Expect(0);
+    example_clock_low_Expect();
+    example_clock_high_Expect();
+
+  }
+  ds1620_send_command(0,&callbacks);
+}
+
+void test_ds1620_send_command_should_work_all_ones(void)
+{
+  for(uint8_t i=0;i<8;++i)
+  {
+    example_dq_set_Expect(1);
+    example_clock_low_Expect();
+    example_clock_high_Expect();
+
+  }
+  ds1620_send_command(0xFF,&callbacks);
+}
+
+void test_ds1620_send_command_should_work(void)
+{
+  for(uint8_t i=0;i<8;++i)
+  {
+    if(i%2)
+      example_dq_set_Expect(1);
+    else
+      example_dq_set_Expect(0);
+    example_clock_low_Expect();
+    example_clock_high_Expect();
+
+  }
+  ds1620_send_command(0xAA,&callbacks);
 }
